f29ebe91578b9b20dcd38061c44ead45aa622f89
Paul pannetier <leonard.pannetier@laposte.net>
2022-11-08 20:59:51 +0100

    l

--- Assets/Scripts/Gameplay/Map/MovablePlatefrom.cs
+++ Assets/Scripts/Gameplay/Map/MovablePlatefrom.cs
@@ -6,24 +6,34 @@ public class MovablePlatefrom : MonoBehaviour
 {
     private enum PlateformeSide
     {
-        Up, Down, Left, Right
+        None = 0, Up = 1, Down = 2, Left = 3, Right = 4
     }
 
+    private static Vector2[] convertSideToDir = new Vector2[5]
+    {
+        Vector2.zero, Vector2.up, Vector2.down, Vector2.left, Vector2.right 
+    };
+
     private BoxCollider2D hitbox;
+    private Rigidbody2D rb;
     private List<Movement> charInFrontLastFrame;
     private Movement charWhoActivate = null;
     private bool isMoving = false;
+    private Vector2 moveDir;
 
     public bool enableBehaviour = true;
 
     [SerializeField] private bool enableLeft = true, enableRight = true, enableUp = true, enableDown = true;
+    [SerializeField] private float speed;
+    [SerializeField, Tooltip("lerp de la vitesse en  %age vMax / sec")] private float speedLerp = 0.6f;
     [SerializeField] private float detectionColliderScale = 1.1f;
     [SerializeField] private float minSpeedToTriggerMovement = 1f;
-    [SerializeField] private LayerMask charMask;
+    [SerializeField] private LayerMask charMask, groundMask;
 
     private void Awake()
     {
         hitbox = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Start()
@@ -49,16 +59,17 @@ public class MovablePlatefrom : MonoBehaviour
             {
                 if (charMov.isDashing)
                 {
-                    Rigidbody2D rb = charMov.GetComponent<Rigidbody2D>();
+                    Rigidbody2D charRb = charMov.GetComponent<Rigidbody2D>();
                     BoxCollider2D charHitbox = charMov.GetComponent<BoxCollider2D>();
                     Vector2 charCenter = (Vector2)charMov.transform.position + charHitbox.offset;
                     Vector2 closestPoint = charHitbox.ClosestPoint(charCenter);
 
-                    if ((closestPoint - charCenter).Dot(rb.velocity) > 0f)
+                    if ((closestPoint - charCenter).Dot(charRb.velocity) > 0f)
                     {
                         //on calcule quelle face de la plateforme est touché
-                        if(CanMovePlateform(charCenter, charHitbox.size, rb.velocity, out PlateformeSide plateformeSide))
+                        if(CanMovePlateform(charCenter, charHitbox.size, charRb.velocity, out PlateformeSide plateformeSide))
                         {
+                            moveDir = convertSideToDir[(int)plateformeSide];
                             isMoving = true;
                             charWhoActivate = charMov;
                             break;
@@ -67,89 +78,105 @@ public class MovablePlatefrom : MonoBehaviour
                 }
             }
             charInFrontLastFrame = charInFront;
-        }
-        
-    }
 
-    private bool CanMovePlateform(in Vector2 center, in Vector2 size, in Vector2 speed, out PlateformeSide plateformeSide)
-    {
-        bool isInXcoord = center.x >= transform.position.x - hitbox.size.x * 0.5f &&
-                        center.x <= transform.position.x + hitbox.size.x * 0.5f;
-        bool isInYcoord = center.y >= transform.position.y - hitbox.size.y * 0.5f &&
-            center.y <= transform.position.y + hitbox.size.y * 0.5f;
-
-        Vector2 topLeft  = new Vector2(center.x - size.x * 0.5f, center.y + size.y * 0.5f);
-        Vector2 topRight = new Vector2(center.x + size.x * 0.5f, center.y + size.y * 0.5f);
-        Vector2 botLeft  = new Vector2(center.x - size.x * 0.5f, center.y - size.y * 0.5f);
-        Vector2 botRight = new Vector2(center.x + size.x * 0.5f, center.y - size.y * 0.5f);
-
-        if (speed.x >= 0f)
-        {
-            if (speed.y >= 0f)
+            bool CanMovePlateform(in Vector2 center, in Vector2 size, in Vector2 speed, out PlateformeSide plateformeSide)
             {
-                //gauche ou bas
-                if (isInXcoord)
+                void IsPointInCoor(in Vector2 point, out bool xCoord, out bool yCoord)
                 {
-                    plateformeSide = PlateformeSide.Down;
-                    return speed.y >= minSpeedToTriggerMovement;
+                    xCoord = point.x >= transform.position.x - hitbox.size.x * 0.5f * detectionColliderScale &&
+                        point.x <= transform.position.x + hitbox.size.x * 0.5f * detectionColliderScale;
+                    yCoord = point.y >= transform.position.y - hitbox.size.y * 0.5f * detectionColliderScale &&
+                        point.y <= transform.position.y + hitbox.size.y * 0.5f * detectionColliderScale;
                 }
-                else if (isInYcoord)
+
+                Vector2 topLeft = new Vector2(center.x - size.x * 0.5f, center.y + size.y * 0.5f);
+                Vector2 topRight = new Vector2(center.x + size.x * 0.5f, center.y + size.y * 0.5f);
+                Vector2 bottomLeft = new Vector2(center.x - size.x * 0.5f, center.y - size.y * 0.5f);
+                Vector2 bottomRight = new Vector2(center.x + size.x * 0.5f, center.y - size.y * 0.5f);
+                bool isInXCoord, isInYCoord;
+                if (speed.x >= 0f)
                 {
-                    plateformeSide = PlateformeSide.Left;
-                    return speed.x >= minSpeedToTriggerMovement;
+                    if (speed.y >= 0f)
+                    {
+                        //gauche ou bas
+                        IsPointInCoor(topRight, out isInXCoord, out isInYCoord);
+                        if (isInXCoord && isInYCoord)
+                        {
+                            plateformeSide = speed.x >= speed.y ? PlateformeSide.Left : PlateformeSide.Down;
+                            return Mathf.Max(speed.x, speed.y) >= minSpeedToTriggerMovement;
+                        }
+                        if (isInXCoord || isInYCoord)
+                        {
+                            plateformeSide = isInXCoord ? PlateformeSide.Down : PlateformeSide.Left;
+                            return isInXCoord ? speed.y >= minSpeedToTriggerMovement : speed.x >= minSpeedToTriggerMovement;
+                        }
+                        plateformeSide = PlateformeSide.None;
+                        return false;
+                    }
+                    else
+                    {
+                        IsPointInCoor(bottomRight, out isInXCoord, out isInYCoord);
+                        if (isInXCoord && isInYCoord)
+                        {
+                            plateformeSide = speed.x >= -speed.y ? PlateformeSide.Left : PlateformeSide.Up;
+                            return Mathf.Max(speed.x, -speed.y) >= minSpeedToTriggerMovement;
+                        }
+                        if (isInXCoord || isInYCoord)
+                        {
+                            plateformeSide = isInXCoord ? PlateformeSide.Up : PlateformeSide.Left;
+                            return isInXCoord ? -speed.y >= minSpeedToTriggerMovement : speed.x >= minSpeedToTriggerMovement;
+                        }
+                        plateformeSide = PlateformeSide.None;
+                        return false;
+                    }
                 }
                 else
                 {
-
-                }
-            }
-            else
-            {
-                //gauche ou haut
-                if (isInXcoord)
-                {
-                    plateformeSide = PlateformeSide.Up;
-                    return -speed.y >= minSpeedToTriggerMovement;
-                }
-                else if (isInYcoord)
-                {
-                    plateformeSide = PlateformeSide.Left;
-                    return speed.x >= minSpeedToTriggerMovement;
+                    if (speed.y >= 0f)
+                    {
+                        IsPointInCoor(topLeft, out isInXCoord, out isInYCoord);
+                        if (isInXCoord && isInYCoord)
+                        {
+                            plateformeSide = -speed.x >= speed.y ? PlateformeSide.Right : PlateformeSide.Down;
+                            return Mathf.Max(-speed.x, speed.y) >= minSpeedToTriggerMovement;
+                        }
+                        if (isInXCoord || isInYCoord)
+                        {
+                            plateformeSide = isInXCoord ? PlateformeSide.Down : PlateformeSide.Right;
+                            return isInXCoord ? speed.y >= minSpeedToTriggerMovement : -speed.x >= minSpeedToTriggerMovement;
+                        }
+                        plateformeSide = PlateformeSide.None;
+                        return false;
+                    }
+                    else
+                    {
+                        IsPointInCoor(bottomLeft, out isInXCoord, out isInYCoord);
+                        if (isInXCoord && isInYCoord)
+                        {
+                            plateformeSide = -speed.x >= -speed.y ? PlateformeSide.Right : PlateformeSide.Up;
+                            return Mathf.Max(-speed.x, -speed.y) >= minSpeedToTriggerMovement;
+                        }
+                        if (isInXCoord || isInYCoord)
+                        {
+                            plateformeSide = isInXCoord ? PlateformeSide.Up : PlateformeSide.Left;
+                            return isInXCoord ? -speed.y >= minSpeedToTriggerMovement : -speed.x >= minSpeedToTriggerMovement;
+                        }
+                        plateformeSide = PlateformeSide.None;
+                        return false;
+                    }
                 }
             }
         }
         else
         {
-            if (speed.y >= 0f)
+            rb.velocity = Vector2.MoveTowards(rb.velocity, moveDir * speed, speedLerp * speed * Time.deltaTime);
+            Collider2D[] cols = PhysicsToric.OverlapBoxAll(transform.position, hitbox.size * detectionColliderScale, 0f, groundMask);
+            foreach (Collider2D col in cols)
             {
-                //droite ou bas
-                if (isInXcoord)
-                {
-                    plateformeSide = PlateformeSide.Down;
-                    return speed.y >= minSpeedToTriggerMovement;
-                }
-                else if (isInYcoord)
-                {
-                    plateformeSide = PlateformeSide.Right;
-                    return -speed.x >= minSpeedToTriggerMovement;
-                }
-            }
-            else
-            {
-                //droite ou haut
-                if (isInXcoord)
-                {
-                    plateformeSide = PlateformeSide.Up;
-                    return -speed.y >= minSpeedToTriggerMovement;
-                }
-                else if (isInYcoord)
-                {
-                    plateformeSide = PlateformeSide.Right;
-                    return -speed.x >= minSpeedToTriggerMovement;
-                }
+
             }
         }
-    }
+    }    
 
     private void OnValidate()
     {
