163317602c1afb370ae1841db269322a3b935d51
Léonard P <leonard.pannetier@laposte.net>
2023-03-11 22:48:52 +0100

    l

---
+++ Assets/Scripts/Gameplay/Player/Fight/Attack/PassifAttack/TransparencyPassif.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Movement), typeof(SpriteRenderer))]
+public class TransparencyPassif : PassifAttack
+{
+    private Movement movement;
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField, Range(0f, 1f)] private float minTransparency = 0.3f;
+    [SerializeField] private float transitionTime;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        movement = GetComponent<Movement>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        spriteRenderer.color = spriteRenderer.color * 1f;
+    }
+
+    protected override void Update()
+    {
+        if (!enableBehaviour)
+            return;
+
+        base.Update();
+        float target = movement.isGrounded || movement.onWall ? 1f : minTransparency;
+        float current = Mathf.MoveTowards(spriteRenderer.color.a, target, ((1f - minTransparency) / transitionTime) * Time.deltaTime);
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, current);
+    }
+
+    private void OnValidate()
+    {
+        transitionTime = Mathf.Max(transitionTime, 0f);
+    }
+}
