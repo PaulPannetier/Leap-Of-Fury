5d6fcbc2c5edf292f75d95b61d3e49300a7ee0bb
PaulPannetier <leonard.pannetier@laposte.net>
2023-04-19 17:58:58 +0200

    l

--- Assets/Scripts/UI/Selection Char/CharHelpCanvas.cs
+++ Assets/Scripts/UI/Selection Char/CharHelpCanvas.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Video;
 
 public class CharHelpCanvas : MonoBehaviour
 {
@@ -7,18 +9,35 @@ public class CharHelpCanvas : MonoBehaviour
     private ControllerType controllerType;
     private int id;
     private Action<int> callbackCloseHelpCanvas;
+    private VideoPlayer videoPlayer;
+    private TextMeshProUGUI explicationText;
+    private int selectedHelpIndex;
+    private AttackVideoData selectedData => helpData[selectedHelpIndex];
 
     [SerializeField] private Vector2 turningSelectorOffset;
-    [SerializeField] private KeyCode closeButtonKeyboard;
-    [SerializeField] private KeyCode closeButtonGamepad1;
-    [SerializeField] private KeyCode closeButtonGamepad2;
-    [SerializeField] private KeyCode closeButtonGamepad3;
-    [SerializeField] private KeyCode closeButtonGamepad4;
+    [SerializeField] private GeneralInput closeHelpCanvasInput;
+    [SerializeField] private GeneralInput nextHelpInput, previousHelpInput;
+
+    [Header("Content")]
+    [SerializeField] private AttackVideoData[] helpData;
+
+    private void Awake()
+    {
+        videoPlayer = GetComponentInChildren<VideoPlayer>();
+        explicationText = GetComponentInChildren<TextMeshProUGUI>();  
+    }
+
+    private void Start()
+    {
+        selectedHelpIndex = 0;
+        OnUpdateUI();
+    }
 
     public void Lauch(TurningSelector turningSelector, ControllerType controllerType, int id, Action<int> callbackCloseHelpCanvas)
     {
         this.turningSelector = turningSelector;
         this.controllerType = controllerType;
+        closeHelpCanvasInput.controllerType = controllerType;
         transform.position = turningSelector.center + turningSelectorOffset;
         this.id = id;
         this.callbackCloseHelpCanvas = callbackCloseHelpCanvas;
@@ -26,41 +45,114 @@ public class CharHelpCanvas : MonoBehaviour
 
     private void Update()
     {
-        if(IsPressingEscape())
+        if(closeHelpCanvasInput.IsPresseDown())
         {
             callbackCloseHelpCanvas.Invoke(id);
             Destroy(gameObject);
         }
 
-        bool IsPressingEscape()
+        if(nextHelpInput.IsPresseDown())
         {
-            switch (controllerType)
-            {
-                case ControllerType.Keyboard:
-                    return CustomInput.GetKeyDown(KeyCode.Escape) || CustomInput.GetKeyDown(KeyCode.H);
-                case ControllerType.Gamepad1:
-                    return CustomInput.GetKeyDown(closeButtonGamepad1);
-                case ControllerType.Gamepad2:
-                    return CustomInput.GetKeyDown(closeButtonGamepad2);
-                case ControllerType.Gamepad3:
-                    return CustomInput.GetKeyDown(closeButtonGamepad3);
-                case ControllerType.Gamepad4:
-                    return CustomInput.GetKeyDown(closeButtonGamepad4);
-                case ControllerType.GamepadAll:
-                    return CustomInput.GetKeyDown(closeButtonGamepad1) || CustomInput.GetKeyDown(closeButtonGamepad2) ||
-                        CustomInput.GetKeyDown(closeButtonGamepad3) || CustomInput.GetKeyDown(closeButtonGamepad4);
-                case ControllerType.All:
-                    return CustomInput.GetKeyDown(KeyCode.Escape) || CustomInput.GetKeyDown(KeyCode.H) || CustomInput.GetKeyDown(closeButtonGamepad1) || CustomInput.GetKeyDown(closeButtonGamepad2) ||
-                            CustomInput.GetKeyDown(closeButtonGamepad3) || CustomInput.GetKeyDown(closeButtonGamepad4);
-                default:
-                    return false;
-            }
+            selectedHelpIndex = (selectedHelpIndex + 1) % helpData.Length;
+            OnUpdateUI();
+        }
+
+        if (previousHelpInput.IsPresseDown())
+        {
+            selectedHelpIndex = (selectedHelpIndex - 1);
+            if (selectedHelpIndex < 0)
+                selectedHelpIndex = helpData.Length - 1;
+            OnUpdateUI();
         }
     }
+
+    private void OnUpdateUI()
+    {
+        videoPlayer.clip = selectedData.video;
+        videoPlayer.Play();
+        explicationText.text = selectedData.description;
+    }
+
+    #region Gizmos/OnValidate
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
         Circle.GizmosDraw((Vector2)transform.position - turningSelectorOffset, 50f);
     }
+
+    #endregion
+
+    #region Custom struct
+
+    [Serializable]
+    private struct AttackVideoData
+    {
+        public VideoClip video;
+        public string description;
+
+        public AttackVideoData(VideoClip video, string description)
+        {
+            this.video = video;
+            this.description = description;
+        }
+    }
+
+    [Serializable]
+    private struct GeneralInput
+    {
+        public KeyCode[] keysKeyboard, keyGamepad1, keyGamepad2, keyGamepad3, keyGamepad4;
+        public ControllerType controllerType;
+
+        public GeneralInput(KeyCode[] keysKeyboard, KeyCode[] keyGamepad1, KeyCode[] keyGamepad2, KeyCode[] keyGamepad3, KeyCode[] keyGamepad4, ControllerType controllerType)
+        {
+            this.keysKeyboard = keysKeyboard;
+            this.keyGamepad1 = keyGamepad1;
+            this.keyGamepad2 = keyGamepad2;
+            this.keyGamepad3 = keyGamepad3;
+            this.keyGamepad4 = keyGamepad4;
+            this.controllerType = controllerType;
+        }
+
+        private bool isKeySomething(Func<KeyCode, bool> func)
+        {
+            switch (controllerType)
+            {
+                case ControllerType.Keyboard:
+                    return GetKeySomething(func, keysKeyboard);
+                case ControllerType.Gamepad1:
+                    return GetKeySomething(func, keyGamepad1);
+                case ControllerType.Gamepad2:
+                    return GetKeySomething(func, keyGamepad2);
+                case ControllerType.Gamepad3:
+                    return GetKeySomething(func, keyGamepad3);
+                case ControllerType.Gamepad4:
+                    return GetKeySomething(func, keyGamepad4);
+                case ControllerType.GamepadAll:
+                    return GetKeySomething(func, keyGamepad1) || GetKeySomething(func, keyGamepad2)
+                        || GetKeySomething(func, keyGamepad3) || GetKeySomething(func, keyGamepad4);
+                case ControllerType.All:
+                    return GetKeySomething(func, keysKeyboard) || GetKeySomething(func, keyGamepad1) || GetKeySomething(func, keyGamepad2)
+                        || GetKeySomething(func, keyGamepad3) || GetKeySomething(func, keyGamepad4);
+                default:
+                    return false;
+            }
+
+            bool GetKeySomething(Func<KeyCode, bool> func, KeyCode[] keyCodes)
+            {
+                foreach (KeyCode key in keyCodes)
+                {
+                    if (func(key))
+                        return true;
+                }
+                return false;
+            }
+        }
+
+        public bool IsPresseDown() => isKeySomething((KeyCode key) => CustomInput.GetKeyDown(key));
+        public bool IsPressedUp() => isKeySomething((KeyCode key) => CustomInput.GetKeyUp(key));
+        public bool IsPressed() => isKeySomething((KeyCode key) => CustomInput.GetKey(key));
+    }
+
+    #endregion
 }
